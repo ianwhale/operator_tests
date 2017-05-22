da707e1293ff075476ce3129cfeb4a0463b7453f
ianwhale <ianwhale@github.com>
2017-05-22 15:28:34 -0400

    Added deletion and replacement 'mutation' methods.

--- src/operator_tests/Mutator.java
+++ src/operator_tests/Mutator.java
@@ -10,12 +10,12 @@ import org.objectweb.asm.*;
 import org.objectweb.asm.tree.*;
 
 public class Mutator implements Opcodes {
-    private static final String CLASSFILE = "/home/ian/eclipse_projects/operator_tests/bin/operator_tests/Test.class";
+    private static final String CLASSFILE = "/home/ian/gi/operator_tests/out/production/operator_tests/operator_tests/Test.class";
     private static final File FILEOBJ = new File(CLASSFILE);
     private static final Random rand = new Random();
 
     /**
-     * Simulates an insertion "mutation".
+     * Simulates "mutation".
      * The only change for more randomness would be to choose a random instruction.
      *      -- This would require a little more work to handle all instructions.
      *      -- However, if we only want to mutate in existing code in the file, this would be much easier (InsnNode implements Clone).
@@ -30,9 +30,14 @@ public class Mutator implements Opcodes {
 		List<MethodNode> methods = visitor.methods;
         for (MethodNode method : methods) {
             if (! method.name.equals("<init>")) { // Ignore constructor (nothing going on).
-                // Insert an instruction at a random place.
-                int position = rand.nextInt(method.instructions.size());
-                method.instructions.insert(method.instructions.get(position), new InsnNode(ICONST_3)); // ICONST_3 means "push a 3 on the stack".
+                switch(rand.nextInt(3)) {
+                    case 0:
+                        Mutator.insertion(method);
+                    case 1:
+                        Mutator.deletion(method);
+                    case 2:
+                        Mutator.replacement(method);
+                }
             }
         }
 
@@ -42,7 +47,71 @@ public class Mutator implements Opcodes {
 		FileOutputStream out = new FileOutputStream("./" + FILEOBJ.getName());
 		out.write(writer.toByteArray());
 		out.close();
+    }
 
-		Runtime.getRuntime().exec("java Test");
+    /**
+     * Insertion mutation, insert an instruction after a random index.
+     * @param method MethodNode
+     */
+    private static void insertion(MethodNode method) {
+        Mutator.insertion(method, rand.nextInt(method.instructions.size()));
+    }
+
+    /**
+     * Perform an insertion after a specific position.
+     *
+     * TODO:: Choose a random instruction (consider how to choose args for multi-argument instructions).
+     *
+     * @param method MethodNode
+     * @param position int
+     */
+    public static void insertion(MethodNode method, int position) {
+        method.instructions.insert(method.instructions.get(position), new InsnNode(ICONST_3));
+    }
+
+    /**
+     * Deletes an instruction at a random position.
+     * @param method MethodNode
+     */
+    public static void deletion(MethodNode method) {
+        Mutator.deletion(method, rand.nextInt(method.instructions.size()));
+    }
+
+    /**
+     * Deletes an instruction at the given position.
+     * @param method MethodNode
+     * @param position int
+     */
+    public static void deletion(MethodNode method, int position) {
+        method.instructions.remove(method.instructions.get(position));
+    }
+
+    /**
+     * Replace a random instruction with an instruction found in some other part of the method.
+     * @param method MethodNode
+     */
+    public static void replacement(MethodNode method) {
+        Mutator.replacement(method, rand.nextInt(method.instructions.size()));
+    }
+
+    /**
+     * Replace an instruction at the given position with an instruction found randomly somewhere else inside method.
+     * @param method MethodNode
+     * @param position int
+     */
+    public static void replacement(MethodNode method, int position) {
+        AbstractInsnNode instruction = method.instructions.get(rand.nextInt(method.instructions.size()));
+        Mutator.replacement(method, position, instruction);
+    }
+
+    /**
+     * Replace an instruction at the given position with the given instruction (does not have to be from inside method).
+     *      -- Unknown interaction: instruction passed in is a pointer to the original rather than a clone.
+     * @param method MethodNode
+     * @param position int
+     * @param instruction AbstractInsnNode, should be cloned (?)
+     */
+    public static void replacement(MethodNode method, int position, AbstractInsnNode instruction) {
+        method.instructions.set(method.instructions.get(position), instruction);
     }
 }
--- src/operator_tests/Test.java
+++ src/operator_tests/Test.java
@@ -2,12 +2,22 @@ package operator_tests;
 
 public class Test {
 	public static void main(String[] args) {
-		int x = 1;
-		int y = 2;
-		int z = 3;
-		int a = x;
-		a = a + y;
-		a = a + z;
+		int a = 1;
+		int b = 15;
+		int c = 3;
+
+		if (a + b <= 30) {
+		    c = 4;
+        }
+        else {
+		    a = 0;
+        }
+
+        while (c > 0) {
+		    a = a + 1;
+		    c = c - 1;
+        }
+
 		System.out.println(a);
 	}
 }
